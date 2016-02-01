831ae23c31963d55c60cfc8f3cc269b0ae35a916
Rolando Cosme <rolandocosme@Rolandos-MBP.home>
2016-01-31 20:27:23 -0500

    corrected zoo function syntax and cleaned unused code

--- index.js
+++ index.js
@@ -22,30 +22,36 @@ connection.connect ( function (err){
   // }
 });
 
-var zoo = function welcome () {
-  console.log( "Welcome to the Zoo and Friends App~!");
-  var menu = ["A", "U", "V", "D"];
-    console.log("Enter (A): ----> To Add a new animal to the Zoo!");
-    console.log("");    
-    console.log("Enter (U): ----> To Update information on an animal in the Zoo!");
-    console.log("");
-    console.log("Enter (V): ----> To Visit the animals in the Zoo!");
-    console.log("");
-    console.log("Enter (D): ----> To Quit and Exit Zoo!");
-    var addAnimals = function () {
-      currentScope = input_scope;
-      console.log("To add an animal to the Zoo please fill out the following form for us!");
-      prompt.get(["name", "type", "age"]), function (err, result){
-        connection.query('INSERT INTO zoo_db (name, type, age) VALUES (?,?,?)', ["Jin", "Bear", "3"], function(err,result){
-          if (err) throw err;
-          menu;
+zoo = function () {
+  var welcome = function ( ){
+    console.log( "Welcome to the Zoo and Friends App~!");
+    var menu = ["A", "U", "V", "D"];
+      console.log("Enter (A): ----> To Add a new animal to the Zoo!");
+      console.log("");    
+      console.log("Enter (U): ----> To Update information on an animal in the Zoo!");
+      console.log("");
+      console.log("Enter (V): ----> To Visit the animals in the Zoo!");
+      console.log("");
+      console.log("Enter (D): ----> To Quit and Exit Zoo!");
+  }
+  var addAnimals = function () {
+    currentScope = input_scope;
+    console.log("To add an animal to the Zoo please fill out the following form for us!");
+    prompt.get(["name", "type", "age"]), function (err, result){
+      connection.query('INSERT INTO zoo_db (name, type, age) VALUES (?,?,?)', ["Jin", "Bear", "3"], function(err,result){
+        if (err) throw err;
+        menu;
       });
     }
-    var visit = function ()
   }
+  var visit = function (){
+    
+  }  
+  welcome();
+  addAnimals();
 }
 
 
 debugger;
 zoo ();
-connection.end();
+//connection.end();
